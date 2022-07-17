9558cf5d9f916c61b4272fa7aaed82e907a99aee
neomujua <neomujua@kakao.com>
2022-07-17 18:52:37 +0900

    저항

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,9 +1,12 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(13, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(digitalRead(13)==HIGH){
+    Serial.println("1");
+  } else if (digitalRead(13)==LOW) {
+    Serial.println("0");
+  }
 }
