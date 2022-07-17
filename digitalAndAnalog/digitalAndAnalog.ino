90c21463cba4a67e5c324b22ecff4d8571e3598a
neomujua <neomujua@kakao.com>
2022-07-17 17:44:11 +0900

    아날로그 방식

--- digitalAndAnalog/digitalAndAnalog.ino
+++ digitalAndAnalog/digitalAndAnalog.ino
@@ -6,4 +6,14 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  for(int i=0; i<256; i++) {
+    analogWrite(3, i);
+    delay(10);
+  }
+
+  for(int i=255; i>0; i--) {
+    analogWrite(3,i);
+    delay(10);
+  }
+  
 }
